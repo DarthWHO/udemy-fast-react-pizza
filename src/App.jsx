98b0c8ec9c99932080a00e0af53893f81fe3ff07
Scott Tibbo <40707785+DarthWHO@users.noreply.github.com>
2025-12-18 18:05:57 -0700

    added layout route

--- src/App.jsx
+++ src/App.jsx
@@ -4,32 +4,23 @@ import Cart from "./features/cart/Cart";
 import Menu from "./features/menu/Menu";
 import Order from "./features/order/Order";
 import CreateOrder from "./features/order/CreateOrder";
+import AppLayout from "./ui/AppLayout";
 
 const router = createBrowserRouter([
   {
-    path: "/",
-    element: <Home />,
-  },
-  {
-    path: "/cart",
-    element: <Cart />,
-  },
-  {
-    path: "/menu",
-    element: <Menu />,
-  },
-  {
-    path: "/order/new",
-    element: <CreateOrder />,
-  },
-  {
-    path: "/order/:orderId",
-    element: <Order />,
+    element: <AppLayout />,
+    children: [
+      { path: "/", element: <Home /> },
+      { path: "/cart", element: <Cart /> },
+      { path: "/menu", element: <Menu /> },
+      { path: "/order/new", element: <CreateOrder /> },
+      { path: "/order/:orderId", element: <Order /> },
+    ],
   },
 ]);
 
 function App() {
-  return <RouterProvider router={router} />;
+  return <RouterProvider router={router}> </RouterProvider>;
 }
 
 export default App;
