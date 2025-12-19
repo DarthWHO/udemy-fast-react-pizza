505bae0e21fb53f49e615dac04abbcc2a13ee3b6
Scott Tibbo <40707785+DarthWHO@users.noreply.github.com>
2025-12-19 14:10:23 -0700

    implemented loading screen, error and ability to fetch menu data

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,25 @@
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import Home from "./ui/Home";
 import Cart from "./features/cart/Cart";
-import Menu from "./features/menu/Menu";
+import Menu, { loader as menuLoader } from "./features/menu/Menu";
 import Order from "./features/order/Order";
 import CreateOrder from "./features/order/CreateOrder";
 import AppLayout from "./ui/AppLayout";
+import Error from "./ui/Error";
 
 const router = createBrowserRouter([
   {
     element: <AppLayout />,
+    errorElement: <Error />,
     children: [
       { path: "/", element: <Home /> },
       { path: "/cart", element: <Cart /> },
-      { path: "/menu", element: <Menu /> },
+      {
+        path: "/menu",
+        element: <Menu />,
+        loader: menuLoader,
+        errorElement: <Error />,
+      },
       { path: "/order/new", element: <CreateOrder /> },
       { path: "/order/:orderId", element: <Order /> },
     ],
