8aaf7758950d2a34234023f48f92b9c6d76b3311
Scott Tibbo <40707785+DarthWHO@users.noreply.github.com>
2025-12-20 10:17:16 -0700

    added ability to search and display order from API

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import Home from "./ui/Home";
 import Cart from "./features/cart/Cart";
 import Menu, { loader as menuLoader } from "./features/menu/Menu";
-import Order from "./features/order/Order";
+import Order, { loader as orderLoader } from "./features/order/Order";
 import CreateOrder from "./features/order/CreateOrder";
 import AppLayout from "./ui/AppLayout";
 import Error from "./ui/Error";
@@ -21,7 +21,12 @@ const router = createBrowserRouter([
         errorElement: <Error />,
       },
       { path: "/order/new", element: <CreateOrder /> },
-      { path: "/order/:orderId", element: <Order /> },
+      {
+        path: "/order/:orderId",
+        element: <Order />,
+        loader: orderLoader,
+        errorElement: <Error />,
+      },
     ],
   },
 ]);
